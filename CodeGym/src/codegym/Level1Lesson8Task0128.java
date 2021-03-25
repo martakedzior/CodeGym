3bd0800de7675605a7757efdb6bb79c7d6512bde
Marta Kedzior <marta.kedzior@wp.pl>
2021-03-25 17:01:21 +0100

    Initial commit

---
+++ CodeGym/src/codegym/Level1Lesson8Task0128.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codegym;
+
+/**
+ *
+ * @author Marta
+ */
+public class Level1Lesson8Task0128 {
+    public static void main(String[] args) {
+        suma(2, 2);
+    }
+
+    public static void suma(int a, int b) {
+        int c = a + b;
+        System.out.print(c);
+    }    
+}
