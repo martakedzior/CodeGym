3e5745b54f4c51e406f6af74b4c8e1f4731bf9d5
Marta Kedzior <marta.kedzior@wp.pl>
2021-03-26 15:39:37 +0100

    Task0201

---
+++ CodeGym/src/codegym/Level2Lesson01Task0201.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codegym;
+
+/**
+ *
+ * @author Marta
+ */
+public class Level2Lesson01Task0201 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        print("Javy łatwo się nauczyć!");
+        print("Java jest zorientowana obiektowo!");
+        print("Java jest niezależna od platformy!");
+    }
+
+    public static void print(String s) {
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+    }
+    
+}
